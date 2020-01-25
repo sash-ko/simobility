77cb5d61c51a23ce0bfaa822f30a93d81e564075
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-01-25 10:56:14 +0100

    Refactor data preprocessing

--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -1,30 +1,64 @@
+import logging
 import json
 import argparse
 from shapely.geometry import shape
 from shapely.geometry.polygon import Polygon
-# import shapely.geometry as geometry
 import geopandas as gpd
 import pandas as pd
-from haversine import haversine
 
-# minimum distance between pickup and dropoff (km)
-MIN_DISTANCE = 0.2
-MAX_DISTANCE = 20
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+
+"""
+Preprocess and convert CSV file with historical trip data to feather format.
+Preprocessing means removing trips that are outside of geofence and change column
+names.
+
+Expected CSV structure:
+
+    "tpep_pickup_datetime": datetime, 
+    "tpep_dropoff_datetime": datetime,
+    "passenger_count": integer,
+    "trip_distance": float,
+    "pickup_longitude": float,
+    "pickup_latitude": float,
+    "dropoff_longitude": float,
+    "dropoff_latitude": float
+
+Output file structure:
+    "pickup_datetime",
+    "dropoff_datetime",
+    "passenger_count",
+    "distance",
+    "pickup_lon",
+    "pickup_lat",
+    "dropoff_lon",
+    "dropoff_lat"
+
+"""
 
 
-def read_geofence(file_name) -> Polygon:
-    geofence = None
+def read_polygon(file_name) -> Polygon:
+    """Read polygon fron geojson file and convert to shapely.Geometry"""
+
+    geom = None
     with open(file_name) as f:
-        geofence = json.load(f)
-        geofence = shape(geofence["features"][0]["geometry"])
-    return geofence
+        geom = json.load(f)
+        geom = shape(geom["features"][0]["geometry"])
+
+    return geom
 
 
 def read_data(file_name) -> pd.DataFrame:
-    print(f'Reading file {file_name}...')
+    """Read trip data from CSV file. Reader assumes certain file structure:
+    name and type of columns.
+
+    TODO: allow user to specify column names
+    """
+
+    logging.info(f"Reading file {file_name}...")
 
     data = pd.read_csv(
-        "yellow_feb_1w_trips.csv",
+        file_name,
         usecols=[
             "tpep_pickup_datetime",
             "tpep_dropoff_datetime",
@@ -38,18 +72,25 @@ def read_data(file_name) -> pd.DataFrame:
         parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"],
     )
 
-    print(f"Data shape {data.shape}")
+    logging.info(f"Data shape {data.shape}")
 
     return data
 
 
-def preprocess(data: pd.DataFrame) -> pd.DataFrame:
+def preprocess(
+    data: pd.DataFrame, min_distance: float, max_distance: float
+) -> pd.DataFrame:
+    """Basic preprocessing:
+    - convert distance from miles to km
+    - remove trips that are too long or too short
+    """
+
     # miles to km
     data.trip_distance = data.trip_distance * 1.60934
 
     print(f"Filtering by distance...")
     data = data[
-        (data.trip_distance > MIN_DISTANCE) & (data.trip_distance < MAX_DISTANCE)
+        (data.trip_distance >= min_distance) & (data.trip_distance <= max_distance)
     ]
     print(f"Data shape {data.shape}")
 
@@ -57,16 +98,19 @@ def preprocess(data: pd.DataFrame) -> pd.DataFrame:
 
 
 def filter_by_shape(data: pd.DataFrame, geofence: Polygon) -> pd.DataFrame:
-    print("Creating pickup geometry...")
+    """Remove trips outside of geofence. Filter by pickup and dropoff locations"""
+
+    logging.info("Creating pickup geometry...")
+
     pickup_data = gpd.GeoDataFrame(
         data,
         geometry=gpd.points_from_xy(data["pickup_longitude"], data["pickup_latitude"]),
     )
 
-    print("Filtering by pickups...")
+    logging.info("Filtering by pickups...")
     data = data[pickup_data.geometry.within(geofence)]
 
-    print("Creating dropoff geometry...")
+    logging.info("Creating dropoff geometry...")
     dropoff_data = gpd.GeoDataFrame(
         data,
         geometry=gpd.points_from_xy(
@@ -74,14 +118,19 @@ def filter_by_shape(data: pd.DataFrame, geofence: Polygon) -> pd.DataFrame:
         ),
     )
 
-    print("Filtering by dropoffs...")
+    logging.info("Filtering by dropoffs...")
     data = data[dropoff_data.geometry.within(geofence)]
 
-    print(f"Data shape {data.shape}")
+    logging.info(f"Data shape {data.shape}")
     return data.drop(["geometry"], axis=1)
 
 
 def save_to_feather(data, output_file):
+    """Use feather format to store preprocessed trip data
+
+    Best format to save pandas data
+    https://towardsdatascience.com/the-best-format-to-save-pandas-data-414dca023e0d
+    """
     columns = [
         "pickup_datetime",
         "dropoff_datetime",
@@ -95,10 +144,8 @@ def save_to_feather(data, output_file):
 
     data.columns = columns
 
-    print(f'Saving to {output_file}...')
+    logging.info(f"Saving to {output_file}...")
 
-    # Best format to save pandas data
-    # https://towardsdatascience.com/the-best-format-to-save-pandas-data-414dca023e0d
     data.reset_index(drop=True).to_feather(output_file)
 
 
@@ -108,13 +155,15 @@ if __name__ == "__main__":
     parser.add_argument("--data-file", help="Input CSV file")
     parser.add_argument("--output-file", help='File with results in "feather" format')
     parser.add_argument("--geofence-file", help="GeoJSON file")
+    parser.add_argument("--min-distance", type=int, default=0.2, help="Distance in km")
+    parser.add_argument("--max-distance", type=int, default=20, help="Distance in km")
 
     args = parser.parse_args()
 
     data = read_data(args.data_file)
-    data = preprocess(data)
+    data = preprocess(data, args.min_distance, args.max_distance)
 
-    geofence = read_geofence(args.geofence_file)
+    geofence = read_polygon(args.geofence_file)
     data = filter_by_shape(data, geofence)
 
     save_to_feather(data, args.output_file)

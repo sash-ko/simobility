3bcef3760b11b31dccb39e2a3d475f6695e49444
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-02-23 15:05:43 +0100

    Dont store all bookings forever

--- simobility/core/booking_service.py
+++ simobility/core/booking_service.py
@@ -9,7 +9,6 @@ class BookingService:
     """
 
     def __init__(self, clock: Clock, max_pending_time: int):
-        self._bookings: List[Booking] = []
         self._pending_bookings: Dict[str, Booking] = {}
         self._max_pending_time = max_pending_time
         self.clock = clock
@@ -18,7 +17,6 @@ class BookingService:
         if not booking.is_pending():
             raise Exception("Booking state is not CREATED")
 
-        self._bookings.append(booking)
         self._pending_bookings[booking.id] = booking
 
     def add_bookings(self, bookings: List[Booking]):

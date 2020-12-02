a29ce99e7387d9ba1f21206cd4408bbdd943855e
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-12-02 20:45:15 +0100

    Add template for a grid world simulation

---
+++ examples/grid_world.py
@@ -0,0 +1,16 @@
+import simobility.routers.base_router import BaseRouter
+from simobility.core import Position
+
+
+class CityBlockRouter(BaseRouter):
+    pass
+
+
+class GridWorldDispatcher:
+
+    def step(self):
+        pass
+
+
+def random_cell(width: int, height: int) -> Position:
+    pass

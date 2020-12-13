7c4b9d9043875fce7908f100337d89bd01b49ac7
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-12-13 12:08:57 +0100

    Fix position test

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,10 +1,10 @@
 import pytest
-from simobility.core import Position
+from simobility.core import GeographicPosition
 
 
 def test_position():
-    p1 = Position(-0.39276123046875, 39.444147324430396)
-    p2 = Position(-0.3577423095703125, 39.50404070558415)
+    p1 = GeographicPosition(-0.39276123046875, 39.444147324430396)
+    p2 = GeographicPosition(-0.3577423095703125, 39.50404070558415)
 
     assert pytest.approx(p1.distance(p2), 0.2) == 7.31
     assert p1.distance(p2) == p2.distance(p1)
